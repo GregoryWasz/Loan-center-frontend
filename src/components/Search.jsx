9011dae947bafee9c708bc8bc2bf64448e0737e2
GregWasz <gregory.waszkow@icloud.com>
2021-03-22 00:38:51 +0100

    Added search feature

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,16 +1,50 @@
-import React, { useEffect, useContext } from "react";
+import { Paper, TextField } from "@material-ui/core";
+import React, { useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
-import { UserContext } from "./UserContext";
+import axios from "../api/axios";
 
 export default function Search() {
   const history = useHistory();
-  const { isLoggedIn } = useContext(UserContext);
+  const [results, setResults] = useState([]);
+
+  async function handleSearch(e) {
+    e.preventDefault();
+    setResults([]);
+
+    await axios
+      .get("/search", {
+        params: {
+          product_name: e.target.value,
+        },
+      })
+      .then((response) => {
+        setResults(response.data.search);
+        console.log(response.data.search);
+      })
+      .catch();
+  }
 
   useEffect(() => {
-    if (!isLoggedIn) {
+    const token = localStorage.getItem("token");
+    if (!token) {
       history.push("/");
     }
-  }, [isLoggedIn, history]);
+  }, [history]);
 
-  return <div>TODO SEARCH</div>;
+  return (
+    <>
+      <Paper>
+        <TextField variant="outlined" onChange={handleSearch}></TextField>
+      </Paper>
+      {results.map((result) => {
+        const { category, product_id, product_name } = result;
+        return (
+          <Paper key={product_id}>
+            {category}
+            {product_name}
+          </Paper>
+        );
+      })}
+    </>
+  );
 }
