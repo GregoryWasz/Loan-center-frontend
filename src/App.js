1fbae929da76e986aff77dadb7214bbd91823437
GregWasz <gregory.waszkow@icloud.com>
2021-03-21 16:23:55 +0100

    Added Routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import Admin from "./components/Admin";
+import Search from "./components/Search";
+import Product from "./components/Product";
+import Products from "./components/Products";
+import Logs from "./components/Logs";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { UserContext } from "./components/UserContext";
+import React, { useState, useEffect } from "react";
+import jwtDecode from "jwt-decode";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#000000",
+    },
+    secondary: {
+      main: "#000000",
+    },
+    background: {
+      main: "#000000",
+    },
+  },
+});
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isAdmin, setIsAdmin] = useState(false);
+  const [currentUsername, setCurrentUsername] = useState("");
+
+  useEffect(() => {
+    try {
+      const token = localStorage.getItem("token");
+      const decoded = jwtDecode(token);
+      setIsLoggedIn(true);
+      setIsAdmin(decoded.admin);
+      setCurrentUsername(decoded.name);
+    } catch (e) {
+      setIsLoggedIn(false);
+      setIsAdmin(false);
+      setCurrentUsername("");
+    }
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <ThemeProvider theme={theme}>
+        <UserContext.Provider
+          value={{
+            isLoggedIn,
+            setIsLoggedIn,
+            isAdmin,
+            setIsAdmin,
+            currentUsername,
+            setCurrentUsername,
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Router>
+            <Header />
+            <Switch>
+              <Route exact path="/">
+                <Login />
+              </Route>
+              <Route path="/register">
+                <Register />
+              </Route>
+              <Route path="/admin">
+                <Admin />
+              </Route>
+              <Route path="/search">
+                <Search />
+              </Route>
+              <Route exact path="/product/:id">
+                <Product />
+              </Route>
+              <Route path="/products">
+                <Products />
+              </Route>
+              <Route path="/logs">
+                <Logs />
+              </Route>
+              <Route>Site not found</Route>
+            </Switch>
+          </Router>
+        </UserContext.Provider>
+      </ThemeProvider>
+    </>
   );
 }
 
