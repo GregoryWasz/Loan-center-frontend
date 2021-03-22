f188cc6c2f07685ce6e5f37217a0df08f1671f8b
GregWasz <gregory.waszkow@icloud.com>
2021-03-22 19:08:47 +0100

    Added admin site feature

--- src/components/Admin.jsx
+++ src/components/Admin.jsx
@@ -1,23 +1,186 @@
-import React, { useEffect, useContext } from "react";
+import { Button, Paper, TextField, Typography } from "@material-ui/core";
+import jwtDecode from "jwt-decode";
+import React, { useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
-import { UserContext } from "./UserContext";
+import axios from "../api/axios";
 
 export default function Admin() {
   let history = useHistory();
-  const { isLoggedIn, isAdmin } = useContext(UserContext);
+  const [sources, setSources] = useState([]);
+  const [users, setUsers] = useState([]);
+  const [newSource, setNewSource] = useState("");
+  const [isChanged, setIsChanged] = useState(0);
+
+  const [newProductName, setNewProductName] = useState("");
+  const [newDescription, setNewDescription] = useState("");
+  const [newCategory, setNewCategory] = useState("");
+  const [newModel, setNewModel] = useState("");
+  const [newPrice, setNewPrice] = useState("");
+  const [newProductSource, setNewProductSource] = useState("");
+
+  async function createNewSource(e) {
+    e.preventDefault();
+
+    await axios
+      .post("/source", { source_name: newSource })
+      .then((response) => {
+        setNewSource("");
+        setIsChanged(1);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  async function createNewProduct(e) {
+    e.preventDefault();
+    const payload = {
+      product_name: newProductName,
+      description: newDescription,
+      category: newCategory,
+      model: newModel,
+      price: newPrice,
+      source: newProductSource,
+    };
+
+    await axios
+      .post("/product", payload)
+      .then((response) => {
+        console.log("new product created");
+        setNewProductName("");
+        setNewDescription("");
+        setNewCategory("");
+        setNewModel("");
+        setNewPrice("");
+        setNewProductSource("");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  async function getAllSources() {
+    await axios
+      .get("/source")
+      .then((response) => {
+        setSources(response.data.sources);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  async function getAllUsers() {
+    await axios
+      .get("/user")
+      .then((response) => {
+        setUsers(response.data.users);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
 
   useEffect(() => {
-    if (!isLoggedIn) {
+    try {
+      const token = localStorage.getItem("token");
+      const decoded = jwtDecode(token);
+      if (!token) {
+        history.push("/");
+      }
+      if (!decoded.admin) {
+        history.push("/");
+      }
+    } catch {
       history.push("/");
     }
-    if (!isAdmin) {
-      history.push("/");
-    }
-  }, [isLoggedIn, isAdmin, history]);
+    getAllSources();
+    getAllUsers();
+  }, [history, isChanged]);
 
   return (
     <>
-      <div>TODO GET ALL USERS, TODO ADD SOURCE, TODO GET ALL SOURCES</div>
+      {users.map((user) => {
+        const { admin, user_id, username } = user;
+        return (
+          <Paper key={user_id}>
+            {admin}
+            {username}
+          </Paper>
+        );
+      })}
+      {sources.map((source) => {
+        const { source_id, source_name } = source;
+        return <Paper key={source_id}>{source_name}</Paper>;
+      })}
+      <Paper>
+        <Typography variant="h5">Add new source</Typography>
+        <TextField
+          type="text"
+          id="newSource"
+          label="newSource"
+          variant="outlined"
+          value={newSource}
+          onChange={(e) => setNewSource(e.target.value)}
+        />
+        <Button variant="outlined" onClick={createNewSource}>
+          Create New Source
+        </Button>
+      </Paper>
+      <Paper>
+        <Typography variant="h5">Add new product</Typography>
+        <TextField
+          type="text"
+          id="newProductName"
+          label="newProductName"
+          variant="outlined"
+          value={newProductName}
+          onChange={(e) => setNewProductName(e.target.value)}
+        />
+        <TextField
+          type="text"
+          id="newDescription"
+          label="newDescription"
+          variant="outlined"
+          value={newDescription}
+          onChange={(e) => setNewDescription(e.target.value)}
+        />
+        <TextField
+          type="text"
+          id="newCategory"
+          label="newCategory"
+          variant="outlined"
+          value={newCategory}
+          onChange={(e) => setNewCategory(e.target.value)}
+        />
+        <TextField
+          type="text"
+          id="newModel"
+          label="newModel"
+          variant="outlined"
+          value={newModel}
+          onChange={(e) => setNewModel(e.target.value)}
+        />
+        <TextField
+          type="number"
+          id="newPrice"
+          label="newPrice"
+          variant="outlined"
+          value={newPrice}
+          onChange={(e) => setNewPrice(e.target.value)}
+        />
+        <TextField
+          type="text"
+          id="newProductSource"
+          label="newProductSource"
+          variant="outlined"
+          value={newProductSource}
+          onChange={(e) => setNewProductSource(e.target.value)}
+        />
+        <Button variant="outlined" onClick={createNewProduct}>
+          Create New product
+        </Button>
+      </Paper>
     </>
   );
 }
