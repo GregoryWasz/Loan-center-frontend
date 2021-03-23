29abb333fadd938d0c092e52a4f1aa0c9a2b56a0
GregWasz <gregory.waszkow@icloud.com>
2021-03-23 22:55:51 +0100

    Added style to Logs

--- src/components/Logs.jsx
+++ src/components/Logs.jsx
@@ -1,9 +1,25 @@
-import { Paper } from "@material-ui/core";
+import { Paper, Typography } from "@material-ui/core";
 import React, { useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
 import axios from "../api/axios";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    margin: "1rem",
+    padding: "1rem",
+    display: "flex",
+    justifyContent: "center",
+    flexDirection: "column",
+  },
+  wrapper: {
+    padding: "0.25rem",
+    margin: "0.25rem",
+  },
+}));
 
 export default function Logs() {
+  const classes = useStyles();
   const history = useHistory();
   const [logs, setLogs] = useState([]);
 
@@ -27,19 +43,17 @@ export default function Logs() {
   }, [history]);
 
   return (
-    <>
+    <Paper elevation={10} className={classes.paper}>
+      <Typography variant="h5">History: </Typography>
       {logs.map((log) => {
         const { date, log_id, product_id, product_name, state, username } = log;
         return (
-          <Paper key={log_id}>
-            {date}
-            {product_name}
-            {product_id}
-            {state}
-            {username}
+          <Paper variant="outlined" className={classes.wrapper} key={log_id}>
+            Date: {date}, ID: {product_id}, Product: {product_name}, Username:{" "}
+            {username}, State: {state}.
           </Paper>
         );
       })}
-    </>
+    </Paper>
   );
 }
