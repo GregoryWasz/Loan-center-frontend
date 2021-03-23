9df3ce038dc8df0702521fec6d0c428359646d68
GregWasz <gregory.waszkow@icloud.com>
2021-03-24 00:42:34 +0100

    Added data grid

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,11 +1,47 @@
-import { Button, Paper } from "@material-ui/core";
+import { Paper, Typography } from "@material-ui/core";
 import React, { useEffect, useState } from "react";
-import { useHistory, Link } from "react-router-dom";
+import { useHistory } from "react-router-dom";
+import { DataGrid } from "@material-ui/data-grid";
 import axios from "../api/axios";
+import { makeStyles } from "@material-ui/core/styles";
+import Alert from "@material-ui/lab/Alert";
+
+const useStyles = makeStyles((theme) => ({
+  data: {
+    minWidth: "60rem",
+    minHeight: "20rem",
+    margin: "0.25rem",
+    padding: "0.25rem",
+  },
+  wrapper: {
+    padding: "1rem",
+    margin: "1rem",
+  },
+  tip: {
+    margin: "0.25rem",
+    padding: "0.25rem",
+  },
+}));
 
 export default function Products() {
   const history = useHistory();
   const [products, setProducts] = useState([]);
+  const classes = useStyles();
+
+  function handleDoubleClick(e) {
+    history.push(`/product/${e.id}`);
+  }
+
+  const columns = [
+    { field: "Category", width: 110 },
+    { field: "Product name", width: 145 },
+    { field: "Model" },
+    { field: "Description", width: 130 },
+    { field: "State" },
+    { field: "Price" },
+    { field: "Source" },
+    { field: "Borrowed by", width: 140 },
+  ];
 
   async function getProducts() {
     await axios
@@ -28,34 +64,20 @@ export default function Products() {
 
   return (
     <>
-      {products.map((product) => {
-        const {
-          borrowed_by,
-          category,
-          description,
-          model,
-          price,
-          product_id,
-          product_name,
-          source,
-          state,
-        } = product;
-        return (
-          <Paper key={product_id}>
-            {borrowed_by}
-            {category}
-            {description}
-            {state}
-            {model}
-            {price}
-            {product_name}
-            {source}
-            <Button component={Link} to={`/product/${product_id}`}>
-              GO TO
-            </Button>
-          </Paper>
-        );
-      })}
+      <Paper elevation={10} className={classes.wrapper}>
+        <Typography variant="h5">Our Products</Typography>
+        <DataGrid
+          disableSelectionOnClick={true}
+          onCellDoubleClick={handleDoubleClick}
+          autoHeight={true}
+          className={classes.data}
+          rows={products}
+          columns={columns}
+        />
+        <Alert className={classes.tip} variant="outlined" severity="info">
+          To get product details double click on it!
+        </Alert>
+      </Paper>
     </>
   );
 }
