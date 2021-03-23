57454377c85436af4ddc18ab040412e049bd3b36
GregWasz <gregory.waszkow@icloud.com>
2021-03-23 22:34:05 +0100

    Added Style to Product

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,9 +1,28 @@
-import { Paper, Button } from "@material-ui/core";
+import { Paper, Button, Typography } from "@material-ui/core";
 import React, { useContext, useEffect, useState } from "react";
 import { useHistory, useParams } from "react-router-dom";
 import axios from "../api/axios";
 import { UserContext } from "./UserContext";
 import Alert from "@material-ui/lab/Alert";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    padding: "1rem",
+    margin: "1rem",
+    display: "flex",
+    justifyContent: "center",
+    flexDirection: "column",
+  },
+  wrapper: {
+    padding: "0.25rem",
+    margin: "0.25rem",
+  },
+  ok: { backgroundColor: "#6F926E", margin: "0.1rem" },
+  borrowed: { backgroundColor: "#F5E403", margin: "0.1rem" },
+  broken: { backgroundColor: "#CF5D78", margin: "0.1rem" },
+  delete: { backgroundColor: "#990112", margin: "0.1rem" },
+}));
 
 export default function Product() {
   const history = useHistory();
@@ -14,6 +33,7 @@ export default function Product() {
   const [isError, setIsError] = useState(false);
   const [errorMessage, setErrorMessage] = useState("");
   const { isAdmin } = useContext(UserContext);
+  const classes = useStyles();
 
   async function handleChangeStateToOk(e) {
     e.preventDefault();
@@ -101,33 +121,59 @@ export default function Product() {
   }, [history, id, isChanged]);
 
   return (
-    <>
+    <Paper elevation={10} className={classes.paper}>
+      <Typography className={classes.wrapper} variant="h5">
+        Product Details
+      </Typography>
       {isError && <Alert severity="error">{errorMessage}</Alert>}
-      <Paper>
-        {product.product_id}
-        {product.product_name}
-        {product.category}
-        {product.model}
-        {product.state}
-        {product.source}
-        {product.price}
-        {product.description}
-        {product.borrowed_by}
-        <Button onClick={handleChangeStateToOk}>OK</Button>
-        <Button onClick={handleChangeStateToBorrowed}>BORROW</Button>
-        <Button onClick={handleChangeStateToBroken}>Broken</Button>
-        {isAdmin && <Button onClick={handleDeleteProduct}>DELETE</Button>}
+      <Paper variant="outlined" className={classes.wrapper}>
+        <Typography>Product: {product.product_name}</Typography>
+        <Typography>Category: {product.category}</Typography>
+        <Typography>Model: {product.model}</Typography>
+        <Typography>Actual state: {product.state}</Typography>
+        <Typography>Source: {product.source}</Typography>
+        <Typography>Price: {product.price}</Typography>
+        <Typography>Description: {product.description}</Typography>
+        {product.borrowed_by && (
+          <Typography>Borrowed by: {product.borrowed_by}</Typography>
+        )}
       </Paper>
+      <Typography className={classes.wrapper} variant="h5">
+        Actions
+      </Typography>
+      <Paper variant="outlined" className={classes.wrapper}>
+        <Button className={classes.ok} onClick={handleChangeStateToOk}>
+          OK
+        </Button>
+        <Button
+          className={classes.borrowed}
+          onClick={handleChangeStateToBorrowed}
+        >
+          BORROW
+        </Button>
+        <Button className={classes.broken} onClick={handleChangeStateToBroken}>
+          Broken
+        </Button>
+        {isAdmin && (
+          <Button className={classes.delete} onClick={handleDeleteProduct}>
+            DELETE
+          </Button>
+        )}
+      </Paper>
+      <Typography className={classes.wrapper} variant="h5">
+        History
+      </Typography>
       {logs.map((log) => {
         const { date, log_id, state, username } = log;
         return (
-          <Paper key={log_id}>
-            {date}
-            {state}
-            {username}
+          <Paper variant="outlined" key={log_id} className={classes.wrapper}>
+            <Typography>{date}</Typography>
+            <Typography>
+              {state}, {username}
+            </Typography>
           </Paper>
         );
       })}
-    </>
+    </Paper>
   );
 }
